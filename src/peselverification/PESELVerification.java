80d2c197cd9098dd4a06b5d49e1742360fd79242
SKapturska <skapturska@us.edu.pl>
2016-11-15 16:15:29 +0100

    Okno

--- src/peselverification/PESELVerification.java
+++ src/peselverification/PESELVerification.java
@@ -3,7 +3,8 @@ public class PESELVerification {
 
 
     public static void main(String[] args) {
-        
+        Window win = new Window();
+        win.setVisible(true);
     }
     
 }
--- src/peselverification/Window.java
+++ src/peselverification/Window.java
@@ -1,14 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package peselverification;
 
-/**
- *
- * @author Sandi
- */
-public class Window {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class Window extends JFrame{
+    
+    JLabel lPesel;
+    JTextField tfPesel;
+    JButton verify;
+    
+    Window() {
+        setTitle("PESEL Verification");
+        setSize(320, 200);
+        setLocation(300, 200);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(null);
+        setResizable(false);
+        
+        lPesel = new JLabel("Podaj nr PESEL: ");
+        lPesel.setLocation(20, 20);
+        lPesel.setSize(100, 25);
+        add(lPesel);
+        
+        tfPesel = new JTextField();
+        tfPesel.setLocation(130, 20);
+        tfPesel.setSize(140,25);
+        add(tfPesel);
+        
+        verify = new JButton("Weryfikuj");
+        verify.setLocation(20, 70);
+        verify.setSize(90,25);
+        add(verify);
+        
+    }
     
 }
